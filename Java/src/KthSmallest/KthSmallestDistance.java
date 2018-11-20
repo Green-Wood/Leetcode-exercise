6413e31d90e4908692f5bad61fa0882f68c5b2b5
Green-Wood <1027572886a@gmail.com>
2018-11-20 10:28:23 +0800

    第k小问题的简化 Nov 19, 2018, 11:24
    Problems：
    1. KthSmallestPrimeFraction
    2. KthSmallestInMatrix
    3. KthSmallestInMultiplyTable
    4. KthSmallestPairsSum
    5. KthSmallestPairDistance
    
    Solutions：
    1. PriorityQueue-based solution with optimization
    2. Sorting-based
    3. Binary search(value range)

---
+++ Java/src/KthSmallest/KthSmallestDistance.java
@@ -0,0 +1,56 @@
+package KthSmallest;
+
+import java.util.Arrays;
+/*
+LeetCode No.719 (CN)
+
+
+给定一个整数数组，返回所有数对之间的第 k 个最小距离。一对 (A, B) 的距离被定义为 A 和 B 之间的绝对差值。
+
+示例 1:
+
+输入：
+nums = [1,3,1]
+k = 1
+输出：0
+解释：
+所有数对如下：
+(1,3) -> 2
+(1,1) -> 0
+(3,1) -> 2
+因此第 1 个最小距离的数对是 (1,1)，它们之间的距离为 0。
+提示:
+
+2 <= len(nums) <= 10000.
+0 <= nums[i] < 1000000.
+1 <= k <= len(nums) * (len(nums) - 1) / 2.
+
+
+
+HINT: Binary Search(Value range)
+ */
+public class KthSmallestDistance {
+    public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int lo = nums[1] - nums[0];
+        for (int i = 0; i < n - 1; i++) {
+            lo = Math.min(lo, nums[i+1] - nums[i]);         // 确定lo与hi值的方法有所不同
+        }
+        int hi = nums[n - 1] - nums[0];
+        for (int cnt = 0; lo < hi; cnt = 0) {
+            int mid = (lo + hi) / 2;
+            for (int i = 0, j = i; i < n ; i++) {                   // 应该从左到右，而非寻常的从右到左
+                while (j < n && nums[j] - nums[i] <= mid) j++;
+                cnt += j - i - 1;
+            }
+            if (cnt < k) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new KthSmallestDistance().smallestDistancePair(new int[]{9, 10, 7, 10, 6, 1, 5, 4, 9, 8}, 18));
+    }
+}
