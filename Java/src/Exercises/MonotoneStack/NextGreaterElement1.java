4454aa8d937226732b133f963f477e8518c3267e
赵文祺 <1027572886a@gmail.com>
2019-02-12 22:52:12 +0800

    NextBiggerElement Feb 12, 2019, 22:52

---
+++ Java/src/Exercises/MonotoneStack/NextGreaterElement1.java
@@ -0,0 +1,50 @@
+package Exercises.MonotoneStack;
+
+import java.util.*;
+
+/*
+给定两个没有重复元素的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。找到 nums1 中每个元素在 nums2 中的下一个比其大的值。
+
+nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。如果不存在，对应位置输出-1。
+
+示例 1:
+
+输入: nums1 = [4,1,2], nums2 = [1,3,4,2].
+输出: [-1,3,-1]
+解释:
+    对于num1中的数字4，你无法在第二个数组中找到下一个更大的数字，因此输出 -1。
+    对于num1中的数字1，第二个数组中数字1右边的下一个较大数字是 3。
+    对于num1中的数字2，第二个数组中没有下一个更大的数字，因此输出 -1。
+示例 2:
+
+输入: nums1 = [2,4], nums2 = [1,2,3,4].
+输出: [3,-1]
+解释:
+    对于num1中的数字2，第二个数组中的下一个较大数字是3。
+    对于num1中的数字4，第二个数组中没有下一个更大的数字，因此输出 -1。
+注意:
+
+nums1和nums2中所有元素是唯一的。
+nums1和nums2 的数组大小都不超过1000。
+ */
+public class NextGreaterElement1 {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int num: nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.poll(), num);
+            }
+            stack.push(num);
+        }
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.getOrDefault(nums1[i], -1);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new NextGreaterElement1().nextGreaterElement(new int[]{2, 4}, new int[]{1, 2, 3, 4})));
+    }
+}
